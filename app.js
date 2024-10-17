a0473818fc7726cfb1e4ed3510d81f32e8c935ab
SyedBashar <MhuammadBashar@gmail.com>
2024-10-17 17:03:45 +0500

    Done JS Task ATM

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("Blance Check kar na ka liya 1 press kara\nWidthrawal ka liya 2press kara\nDeposite kaliya 3press kara\nExit kaliya 3 press kara")
+var input = +prompt("Enter Number")
+if (input === 1){
+    alert("Your Balance Is Rs.40000")
+}
+else if (input === 2){
+    alert("Withdrawal Proccess")
+}
+else if (input === 3){
+    alert("Deposit Proccess")
+}
+else if (input === 4 ){
+    alert("EXIT")
+}
+else {
+    alert("Not Found")
+}
